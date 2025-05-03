cbcab1d79030eb7956f730c638937e4ef0cec63c
Joana De Luna <163531588+joanamariebdeluna@users.noreply.github.com>
2025-05-03 16:29:54 +0800

    Update Salary_Calculator.py

--- Salary_Calculator.py
+++ Salary_Calculator.py
@@ -1,4 +1,3 @@
 def TotalPay_calculator(hourly_rate, hours_worked):
     if hourly_rate < 0 or hours_worked < 0:
-        raise ValueError("Hourly rate and hours worked cannot be negative.")
     return hourly_rate * hours_worked
