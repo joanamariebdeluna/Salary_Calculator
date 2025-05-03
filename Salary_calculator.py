0cb87bfd9caa0f6e43aef03698a8745983e810f7
Renaflormsaura <renaflorsaura4@gmail.com>
2025-05-03 17:22:34 +0800

    Initial commit

--- Salary_calculator.py
+++ Salary_calculator.py
@@ -1,3 +1,4 @@
-def calculate_total_pay(hourly_rate, hours_worked):
+
+def Totalpay_calculator(hourly_rate, hours_worked):
     if hourly_rate < 0 or hours_worked < 0:
         return hourly_rate * hours_worked
---
+++ Salary_calculator_test.py
@@ -0,0 +1,10 @@
+
+from Salary_calculator import Salary_calculator
+
+from Salary_calculator import (
+    Totalpay_calculator,
+)
+
+def Totalpay_calculator_test():
+    assert Totalpay_calculator(15, 40) == 600
+    Totalpay_calculator(-10, 20)
