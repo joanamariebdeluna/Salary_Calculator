f02c600728414a9b3c710fdd3851c5527e8926e3
aiemsbonganay <aiembonganay24@gmail.com>
2025-05-03 21:26:26 +0800

    Initial commit

--- Salary_calculator_test.py
+++ Salary_calculator_test.py
@@ -1,6 +1,4 @@
-
 from Salary_calculator import Salary_calculator
-
 
 from Salary_calculator import (
     Totalpay_calculator,
@@ -8,17 +6,14 @@ from Salary_calculator import (
     Deduction_calculator,
 )
 
-
 def Totalpay_calculator_test():
     assert Totalpay_calculator(15, 40) == 600
     Totalpay_calculator(-10, 20)
-    
-    
+     
 def Tax_calculator():
     assert Tax_calculator(600, 0.2) == 120
     Tax_calculator(600, 1.2)
-    
-    
+     
 def Deduction_calculation():
     assert Deduction_calculation(600, 0.1) == 60
     Deduction_calculation(600, -0.1)
