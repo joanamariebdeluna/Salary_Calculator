0c30f7cfd2ffb756452ea9bcb6eae2b5eb8a10fc
aiemsbonganay <aiembonganay24@gmail.com>
2025-05-03 20:58:42 +0800

    Initial commit

--- Salary_calculator.py
+++ Salary_calculator.py
@@ -3,11 +3,13 @@ def Totalpay_calculator(hourly_rate, hours_worked):
     if hourly_rate < 0 or hours_worked < 0:
         return hourly_rate * hours_worked
 
+
 def Tax_calculator(total_pay, tax_rate):
     if tax_rate < 0 or tax_rate > 1:
         return total_pay * tax_rate
     
-def calculate_deductions(total_pay, deduction_rate):
+    
+def Deduction_calculator(total_pay, deduction_rate):
     if deduction_rate < 0 or deduction_rate > 1:
         return total_pay * deduction_rate
     
--- Salary_calculator_test.py
+++ Salary_calculator_test.py
@@ -1,19 +1,23 @@
 
 from Salary_calculator import Salary_calculator
 
+
 from Salary_calculator import (
     Totalpay_calculator,
     Tax_calculator,
     Deduction_calculator,
 )
 
+
 def Totalpay_calculator_test():
     assert Totalpay_calculator(15, 40) == 600
     Totalpay_calculator(-10, 20)
     
+    
 def Tax_calculator():
     assert Tax_calculator(600, 0.2) == 120
     Tax_calculator(600, 1.2)
+    
     
 def Deduction_calculation():
     assert Deduction_calculation(600, 0.1) == 60
